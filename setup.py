108904a7210f656fdfe7cd5ffee7bff569ce3617
Àlex Cervilla <cervilla.alex@gmail.com>
2021-12-21 18:53:51 +0100

    fix package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="1.0.0",
     author="KeyFr4me",
     description="Network utilities used by KeyFr4me",
-    packages=find_packages(include=["k4net"]),
+    packages=find_packages(),
     install_requires=["pycryptodome>=3.9.9"]
 )
