e2cdefba6aac99118cd7038aa7a228d764de3687
Àlex Cervilla <cervilla.alex@gmail.com>
2022-06-18 12:43:37 +0200

    version_change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="k4net",
-    version="2.0.2",
+    version="3.0.0",
     author="KeyFr4me",
     description="Network utilities used by KeyFr4me",
     packages=find_packages(),
